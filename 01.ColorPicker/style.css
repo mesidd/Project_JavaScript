f54681ee3f4c4d749da490fba3b8e10cf0466397
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-03-25 01:59:01 +0530

    style file

---
+++ 01.ColorPicker/style.css
@@ -0,0 +1,90 @@
+body {
+  background-color: #000;
+  color: white;
+  margin: 0;
+  padding: 0;
+}
+
+p {
+  font-size: 24px;
+}
+
+#color {
+  height: 140px;
+  width: 220px;
+  cursor: pointer;
+  outline: none;
+  background: none;
+  appearance: none;
+  border: 0px;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 30px;
+}
+
+.copy{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(73, 235, 73);
+  height: 46px;
+  width: 200px;
+  color: black;
+  border-radius: 4px;
+  font-size: 24px;
+  cursor: pointer;
+  outline: none;
+}
+
+#save{
+  text-align: center;
+  height: 40px;
+  background-color: rgb(73, 235, 73);
+  border: none;
+  border-radius: 4px;
+  font-size: 20px;
+}
+
+#box {
+  height: 100px;
+  width: 200px;
+  background-color: white;
+  margin-bottom: 30px;
+  border-radius: 4px;
+}
+
+#code{
+  background-color: white;
+  color: black;
+  width: 100px;
+  text-align: center;
+  font-size: 26px;
+}
+
+#comp-heading{
+  font-size: 30px;
+}
+
+#fav-color{
+  margin: 0;
+  padding: 0;
+  display: flex;
+  gap: 10px;
+  list-style: none;
+}
+
+#fav-color div {
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80px;
+  height: 40px;
+  border-radius: 6px ;
+  border: 1px solid white;
+}
